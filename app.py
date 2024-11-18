b4839d6fcb929254cd7cf9b1bde5ad9dceeaee70
zepion1 <samuel_elka99@hotmail.com>
2024-11-17 19:23:20 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def init_sql(connection, init_file = 'init.sql'):
             if query.strip():
                 cursor.execute(query)
         connection.commit()
-        print("Databse initialized!")
+        print("Databse initialized...")
     except FileNotFoundError:
         print(f"Error: File '{init_file}' not found.")
     except mysql.connector.Error as e:
@@ -34,13 +34,64 @@ def init_sql(connection, init_file = 'init.sql'):
     finally:
         cursor.close()
 
+#Enroll students to courses
+def enroll_student(cursor, student_id, connection):
+    if not connection.is_connected():
+        connection.reconnect()
+    # 1. Check prerequisites
+    course_id = input("Enter course ID to enroll in: ")
+    query_prereqs = """ SELECT prerequisite_course_id
+                        FROM prerequisites
+                        WHERE course_id = %s AND prerequisite_course_id 
+                        NOT IN (SELECT course_id
+                                FROM enrollments
+                                WHERE student_id = %s);"""
+    cursor.execute(query_prereqs, (course_id, student_id))
+    unmet_prereqs = cursor.fetchall()
 
-def withdraw_course(cursor, student_id, course_id):
-    query = "DELETE FROM enrollment WHERE StudentID = %s AND CourseID = %s"
-    cursor.execute(query, (student_id, course_id))
-    connection.commit()
-    print("Course withdrawn successfully.")
+    if unmet_prereqs:
+        print("You do not meet all the prerequisites for this course.")
+        for prereq in unmet_prereqs:
+            print(f"- Prerequisite Course ID: {prereq[0]}")
+        return False
 
+    # 2. Check schedule conflicts
+    query_schedule_conflicts = """  SELECT  e.course_id AS conflicting_course_id, s1.meeting_day, s1.start_time, s1.end_time
+                                    FROM enrollments e
+                                    JOIN schedule s1 ON e.course_id = s1.course_id
+                                    JOIN schedule s2 ON s2.course_id = %s
+                                    WHERE e.student_id = %s 
+                                    AND ((s1.meeting_day = s2.meeting_day) 
+                                    AND ((s1.start_time < s2.end_time 
+                                    AND s1.end_time > s2.start_time))); """
+    cursor.execute(query_schedule_conflicts, (course_id, student_id))
+    schedule_conflict = cursor.fetchall()
+    
+    if schedule_conflict:
+        print("This course conflicts with your current schedule.")
+        for conflict in schedule_conflict:
+            print(f"- Conflict with Course ID: {conflict[0]} on {conflict[1]} from {conflict[2]} to {conflict[3]}")
+            return False
+
+    # 3. Check for duplicate enrollment
+    query_duplicate = """SELECT 1
+                         FROM enrollments
+                        WHERE student_id = %s AND course_id = %s; """
+    cursor.execute(query_duplicate, (student_id, course_id))
+    duplicate = cursor.fetchone()
+
+    if duplicate:
+        print("You are already enrolled in this course.")
+        return False
+
+    # 4. Enroll in the course
+    query_enroll = "INSERT INTO enrollments (student_id, course_id) VALUES (%s, %s);"
+    cursor.execute(query_enroll, (student_id, course_id))
+    print(f"Enrollment successful!")
+    print(f"Student ID {student_id} successfully enrolled in Course ID {course_id}.")
+    return False
+
+#List classes from student
 def list_my_classes(cursor, student_id): 
     query = """ SELECT c.course_name, 
                 GROUP_CONCAT(s.meeting_day ORDER BY s.meeting_day SEPARATOR ', ') AS days,
@@ -55,52 +106,7 @@ def list_my_classes(cursor, student_id):
     cursor.execute(query, (student_id,))
     classes = cursor.fetchall()
     for c in classes:
-        print(f"| Course Name: {c[0]} | Day(s): {c[1]} | Start Time: {c[2]} | End Time: {c[3]} | Location: {c[4]} | Professor: {c[5]} |")
-
-#Enroll students to courses
-def enroll_student(cursor, student_id, course_id):
-    # 1. Check prerequisites
-    query_prereqs = """ SELECT prerequisite_course_id
-                        FROM prerequisites
-                        WHERE course_id = %s AND prerequisite_course_id 
-                        NOT IN (SELECT course_id
-                                FROM enrollments
-                                WHERE student_id = %s);"""
-    cursor.execute(query_prereqs, (course_id, student_id))
-    unmet_prereqs = cursor.fetchall()
-    if unmet_prereqs:
-        print("You do not meet all the prerequisites for this course.")
-        return
-
-    # 2. Check schedule conflicts
-    query_schedule_conflicts = """  SELECT 1
-                                    FROM enrollments e
-                                    JOIN schedule s1 ON e.course_id = s1.course_id
-                                    JOIN schedule s2 ON s2.course_id = %s
-                                    WHERE e.student_id = %s 
-                                    AND ((s1.meeting_day = s2.meeting_day) 
-                                    AND ((s1.start_time < s2.end_time 
-                                    AND s1.end_time > s2.start_time))); """
-    cursor.execute(query_schedule_conflicts, (course_id, student_id))
-    schedule_conflict = cursor.fetchone()
-    if schedule_conflict:
-        print("This course conflicts with your current schedule.")
-        return
-
-    # 3. Check for duplicate enrollment
-    query_duplicate = """SELECT 1
-                         FROM enrollments
-                        WHERE student_id = %s AND course_id = %s; """
-    cursor.execute(query_duplicate, (student_id, course_id))
-    duplicate = cursor.fetchone()
-    if duplicate:
-        print("You are already enrolled in this course.")
-        return
-
-    # 4. Enroll in the course
-    query_enroll = "INSERT INTO enrollments (student_id, course_id) VALUES (%s, %s);"
-    cursor.execute(query_enroll, (student_id, course_id))
-    print("Enrollment successful!")
+        print(f"| Course Name: {c[0]} | Day(s): {c[1]} | Start Time: {c[2]} | End Time: {c[3]} | Location: {c[4]} | Professor: {c[5]} |")    
 
 #list all available courses
 def list_courses(cursor): 
@@ -160,12 +166,15 @@ def add_student(cursor, connection):
         (%s, %s, %s, %s);"""
     try:
         cursor.execute(query, (sname, email, major, year))
+        student_id = cursor.lastrowid
         connection.commit()
-        print(f"Student: {sname} has been added to the database!")
-        return
+        print(f"Student: {sname} has been added to the database, Assigned Student ID: {student_id}!")
+        EnterStudentID(cursor, connection)
+        return student_id
     except Exception as e:
         print(f"Error adding Student: {e}")
         connection.rollback()
+        EnterStudentID(cursor, connection)
 
 #Check if Student exist in database
 def check_student(cursor, sid):
@@ -174,59 +183,96 @@ def check_student(cursor, sid):
     check = cursor.fetchone()
     if check:
         print(f"Welcome Back {check[1]}! ")
+        optionsMenu(cursor, connection)
+        return sid
     else:
         print(f"Student with ID: {sid} does not exits.")
+        EnterStudentID(cursor, connection)
+
+
+def withdraw_course(cursor, connection, student_id, course_id):
+    query = "DELETE FROM enrollments WHERE student_id = %s AND course_id = %s"
+    try:
+        if not connection.is_connected():
+            connection.reconnect()
+        cursor.execute("SELECT student_id From students WHERE student_id = %s",(student_id,))
+        if not cursor.fetchone():
+            print(f"Error: Student ID {student_id} does not exist.")
+            return
+        cursor.execute("SELECT course_id From courses WHERE course_id = %s",(course_id,))
+        if not cursor.fetchone():
+            print(f"Error: Course ID: {course_id} does not exist.")
+            return
+        cursor.execute("SELECT * FROM enrollments WHERE student_id = %s AND course_id = %s;",(student_id, course_id))
+        if not cursor.fetchone():
+            print(f"Error: Student ID {student_id} is not enrolled in Course ID {course_id}")
+            return
+        cursor.execute(query,(student_id, course_id))
+        connection.commit()
+        print(f"Student ID {student_id} succesfully withdrew from Course ID {course_id}.")
+    except Exception as e:
+        print(f"Erro: An error occurred withdrawing from the course: {e}")
+        connection.rollback()
+
+#option Menu
+def optionsMenu(cursor, connection):
+    while True:
+            print("\nMenu:")
+            print("L - List Courses")
+            print("E - Enroll in a Course")
+            print("W - Withdraw from a Course")
+            print("S - Search Courses")
+            print("M - My Classes")
+            print("P - Prerequisites")
+            print("T - Teaching Professors")
+            print("X - Exit")
+            choice = input("Select an option: ").upper()
+            if choice == 'L':
+                list_courses(cursor)
+            elif choice == 'E':
+                student_id = int(input("Re-enter your student ID: "))
+                #course_id = int(input("Enter course ID to enroll in: ").split())
+                enroll_student(cursor, student_id, connection)
+            elif choice == 'W':
+                student_id = int(input("Enter your student ID: ").strip())
+                course_id = int(input("Enter course ID to withdraw from: ").strip())
+                withdraw_course(cursor, connection, student_id, course_id)
+            elif choice == 'S':
+                substring = input("\nEnter course name substring: ")
+                search_courses(cursor, substring)
+            elif choice == 'M':
+                student_id = int(input("\nEnter your student ID: "))
+                list_my_classes(cursor, student_id)
+            elif choice == 'P':
+                course_id = int(input("\nEnter course ID to view prerequisites: "))
+                show_prerequisites(cursor, course_id)
+            elif choice == 'T':
+                list_teaching_professors(cursor)
+            elif choice == 'X':
+                print("\nExiting System...")
+                break
+            else:
+                print("\nInvalid option.")
+
+
+def EnterStudentID(cursor, connection):
+    try:
+        sid = int(input("\nEnter Student ID or -1 to sign up: ").strip())
+        if(sid == -1):
+            add_student(cursor, connection)
+        else:
+            check_student(cursor, sid)
+    except ValueError:
+        print("\nPlease Enter a Valid ID.")
+        EnterStudentID(cursor, connection)
+    
 
 def main():
     connection = connect_to_database()
     if not connection:
         return
     cursor = connection.cursor()
-
-    sid = int(input("Enter Student ID or -1 to sign up: ").strip())
-    if(sid == -1):
-        add_student(cursor, connection)
-    else:
-        check_student(cursor, sid)
-
-    while True:
-        print("\nMenu:")
-        print("L - List Courses")
-        print("E - Enroll in a Course")
-        print("W - Withdraw from a Course")
-        print("S - Search Courses")
-        print("M - My Classes")
-        print("P - Prerequisites")
-        print("T - Teaching Professors")
-        print("X - Exit")
-        choice = input("Select an option: ").upper()
-        if choice == 'L':
-            list_courses(cursor)
-        elif choice == 'E':
-            student_id = int(input("Enter your student ID: "))
-            course_id = int(input("Enter course ID to enroll in: "))
-            enroll_student(cursor, student_id, course_id)
-        elif choice == 'W':
-            student_id = int(input("Enter your student ID: "))
-            course_id = int(input("Enter course ID to withdraw from: "))
-            withdraw_course(cursor, student_id, course_id)
-        elif choice == 'S':
-            substring = input("\nEnter course name substring: ")
-            search_courses(cursor, substring)
-        elif choice == 'M':
-            student_id = int(input("\nEnter your student ID: "))
-            list_my_classes(cursor, student_id)
-        elif choice == 'P':
-            course_id = int(input("\nEnter course ID to view prerequisites: "))
-            show_prerequisites(cursor, course_id)
-        elif choice == 'T':
-            list_teaching_professors(cursor)
-        elif choice == 'X':
-            print("Exiting System...")
-            break
-        else:
-            print("Invalid option.")
-
+    EnterStudentID(cursor, connection)
     cursor.close()
     connection.close()
 
